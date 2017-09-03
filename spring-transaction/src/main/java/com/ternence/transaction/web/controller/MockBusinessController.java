9390a874921e96ab15c2551799abd8ba0f75d752
陶江航 <taojianghang@xinzhentech.com>
2017-09-03 17:38:37 +0800

    Add mybatis to support dao layer coding.

--- spring-transaction/src/main/java/com/ternence/transaction/web/controller/MockBusinessController.java
+++ spring-transaction/src/main/java/com/ternence/transaction/web/controller/MockBusinessController.java
@@ -1,10 +1,14 @@
 package com.ternence.transaction.web.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author 陶江航
  * @version 1.0
  * @date 2017/9/3
  * @description 模拟业务实现的Controller
  */
+@RestController
 public class MockBusinessController {
+
 }
