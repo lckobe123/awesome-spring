ef20f0fce09a424d9cac5abc592370c85f51663e
陶江航 <taojianghang@xinzhentech.com>
2017-11-02 11:10:29 +0800

    新增spring boot web项目

---
+++ springboot-web/src/main/java/com/ternence/springboot/MyApplication.java
@@ -0,0 +1,20 @@
+package com.ternence.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * create by 陶江航 at 2017/11/2 10:23
+ *
+ * @version 1.0
+ * @email taojianghang@xinzhentech.com
+ * @description spring boot 启动的一个项目
+ */
+@SpringBootApplication
+public class MyApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
