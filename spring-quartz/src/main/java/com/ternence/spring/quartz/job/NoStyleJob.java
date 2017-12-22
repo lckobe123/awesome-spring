2a9c97ee8fe077469443e5b55e4f2be2290be1de
陶江航 <taojianghang@xinzhentech.com>
2017-12-22 21:09:33 +0800

    新增持久化quartzJob相关数据功能

--- spring-quartz/src/main/java/com/ternence/spring/quartz/job/NoStyleJob.java
+++ spring-quartz/src/main/java/com/ternence/spring/quartz/job/NoStyleJob.java
@@ -3,15 +3,21 @@ package com.ternence.spring.quartz.job;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author Ternence
  * @version 1.0
  */
-public class NoStyleJob {
+public class NoStyleJob implements Serializable {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     public void execute() {
-        logger.info("没有风格的Job");
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        logger.info("没有风格的Job,现在时间为：" + format.format(new Date()));
     }
 }
