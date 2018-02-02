6ee59395f9882ae574473b1923072c5cfc0a3dbf
陶江航 <taojianghang@xinzhentech.com>
2018-02-02 22:51:57 +0800

    为秒杀项目整合redis

--- seckill/src/main/java/com/ternence/seckill/controller/SeckillController.java
+++ seckill/src/main/java/com/ternence/seckill/controller/SeckillController.java
@@ -1,6 +1,16 @@
 package com.ternence.seckill.controller;
 
-import org.springframework.stereotype.Controller;
+import com.ternence.seckill.SeckillApplicationStarter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 秒杀业务的Controller
@@ -9,6 +19,19 @@ import org.springframework.stereotype.Controller;
  * @version 1.0
  * @since 1.0
  */
-@Controller
+@RestController
 public class SeckillController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SeckillApplicationStarter.class);
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @GetMapping("/redis/config/{key}")
+    public Object requestConfig(@PathVariable String key) {
+        LOGGER.info("获取{}对应的值", key);
+        Map<String, Object> data = new HashMap<>();
+        data.put("name", stringRedisTemplate.opsForValue().get(key));
+        return data;
+    }
+
 }
