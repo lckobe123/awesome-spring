703fb4ea1c773b34bb71ed793248cc1fbc7dc231
陶江航 <taojianghang@xinzhentech.com>
2018-02-02 22:05:46 +0800

    新建秒杀项目

---
+++ seckill/src/main/java/com/ternence/seckill/controller/SeckillController.java
@@ -0,0 +1,14 @@
+package com.ternence.seckill.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 秒杀业务的Controller
+ *
+ * @author Ternence
+ * @version 1.0
+ * @since 1.0
+ */
+@Controller
+public class SeckillController {
+}
