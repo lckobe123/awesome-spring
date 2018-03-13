bcce24ddebc34f5b0ebf59c9bc756919b4971d13
陶江航 <taojianghang@xinzhentech.com>
2018-03-13 22:27:15 +0800

    完成redisson分布式锁的简单使用

--- spring-redis/src/main/java/com/ternence/spring/redis/RedisDisLockDemo.java
+++ spring-redis/src/main/java/com/ternence/spring/redis/RedisDisLockDemo.java
@@ -9,39 +9,99 @@ import java.util.concurrent.*;
 
 /**
  * 使用redisson实现的分布式锁 测试效果
+ * <p>
+ * redisson实现的锁并非是互斥的，一个锁可以被很多线程同时获取到,他们也可以同时操作一个资源，这应该是典型的乐观锁的实现
  *
  * @author Ternence
  * @version 1.0
  */
 public class RedisDisLockDemo {
-    private final static String SERVER_ADDRESS = "http://192.168.72.128:6379";
+    private final static String SERVER_ADDRESS = "192.168.72.128:6379";
     private Count count = new Count(1);
-
-    private Runnable task = () -> {
-        Config config = new Config();
-        config.useSingleServer().setAddress(SERVER_ADDRESS);
-        RedissonClient client = Redisson.create(config);
-        //他的锁是用一个hash值作为锁的
-        RLock rLock = client.getLock("lock:bond");
-        rLock.lock();//上锁，这是一个阻塞操作,会一直阻塞程序的执行
-        try {
-            count.countSub();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            rLock.unlock();//释放锁
-        }
-        client.shutdown();//关闭Redisson实例,释放资源
-    };
 
     public static void main(String[] args) {
         RedisDisLockDemo redisDisLockDemo = new RedisDisLockDemo();
-        redisDisLockDemo.testExaminationScene();
+        redisDisLockDemo.testCommonLock();
     }
 
-    public RedisDisLockDemo() {
+
+    private void testCommonLock() {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(100, 1000,
+                1, TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(100));
+
+        //执行一个任务去获取锁,然后加锁,不释放，看看后面还能不能获取到锁
+        executor.execute(() -> {
+            Config config = new Config();
+            config.useSingleServer().setAddress(SERVER_ADDRESS);
+            RedissonClient client = Redisson.create(config);
+            //他的锁是用一个hash值作为锁的,使用ttl观察这个锁的变化发现，生命时间到20的时候回自动变为30，循环变化，key就一直被锁住了
+            //RLock rLock = client.getLock("bond");//这步骤只是创建了一个RLock对象,并非执行了获取锁的操作
+            RLock rLock = client.getLock("bond");//这步骤只是创建了一个RLock对象,并非执行了获取锁的操作
+            //isLocked()观察这把锁是否被任何的线程锁住,逻辑就是如果这个锁存在就是被人锁住了,否则就不是被锁住的
+            //执行获取锁的操作,如果所已经被其他线程持有的话，这里就需要等待,否则获取成功锁，执行后面的逻辑
+            //rLock.lock();//上锁，这个步骤会在redis中产生一个hash值，也就是锁,这是一个阻塞操作,会一直阻塞到锁的释放
+            rLock.lock(3, TimeUnit.SECONDS);//上一个带有超时特性的锁,
+            System.out.println("获取到锁：" + rLock.isLocked());
+        });
+
+        //这里将会产生许多等待的线程,当锁放开之后大约10s左右会有下一个线程获得锁
+        for (int i = 0; i < 10; i++) {
+            executor.execute(() -> {
+                Config config = new Config();
+                config.useSingleServer().setAddress(SERVER_ADDRESS);
+                RedissonClient client = Redisson.create(config);
+                //他的锁是用一个hash值作为锁的,获取一个公平锁
+                RLock rLock = client.getLock("bond");
+                //rLock.lock();//执行获取锁的操作,如果所已经被其他线程持有的话，这里就需要等待,否则获取成功锁，执行后面的逻辑
+                rLock.lock(3, TimeUnit.SECONDS);//上一个带有超时特性的锁
+                System.out.println(Thread.currentThread().getName() + " 获取到锁");
+            });
+        }
+
+        executor.shutdown();//不接受任何新的任务,但是他会等前面的任务有序的执行完毕
     }
 
+
+    private void testDisLock() {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(100, 1000, 1, TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(100));
+
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            Runnable task = () -> {
+                Config config = new Config();
+                config.useSingleServer().setAddress(SERVER_ADDRESS);
+                RedissonClient client = Redisson.create(config);
+                //他的锁是用一个hash值作为锁的,获取一个公平锁
+                RLock rLock = client.getLock("lock:");
+                rLock.lock();//上锁，这是一个阻塞操作,会一直阻塞程序的执行
+                if (rLock.isLocked()) {
+                    try {
+                        count.countSub();
+                        System.out.println(Thread.currentThread().getName() + "执行任务i=" + finalI + " count=" + count.getCount());
+                        Thread.sleep(5 * 1000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        System.out.println(Thread.currentThread().getName() + "任务完成，释放锁");
+                        rLock.unlock();//释放锁
+                        client.shutdown();//关闭Redisson实例,释放资源
+                    }
+                } else {
+                    System.out.println("当前线程" + Thread.currentThread().getName() + "并没有锁定这个资源");
+                }
+
+            };
+            executor.execute(task);
+        }
+
+        executor.shutdown();//终止线程池
+
+    }
+
+
+    @SuppressWarnings("unused")
     private void testExaminationScene() {
         //线程池
         ThreadPoolExecutor executor = new ThreadPoolExecutor(100, 1000, 1, TimeUnit.SECONDS,
--- spring-transaction/src/main/java/com/ternence/transaction/web/controller/MockBusinessController.java
+++ spring-transaction/src/main/java/com/ternence/transaction/web/controller/MockBusinessController.java
@@ -3,10 +3,10 @@ package com.ternence.transaction.web.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * 模拟业务实现的Controller
+ *
  * @author 陶江航
  * @version 1.0
- * @date 2017/9/3
- * @description 模拟业务实现的Controller
  */
 @RestController
 public class MockBusinessController {
