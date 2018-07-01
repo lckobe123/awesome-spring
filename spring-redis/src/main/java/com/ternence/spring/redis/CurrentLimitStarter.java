635fc8dcbc6ce54a7b1a90619f1c947e209b66cd
陶江航 <ternence.tao@foxmail.com>
2018-07-01 23:23:24 +0800

    修正错误代码

--- spring-redis/src/main/java/com/ternence/spring/redis/CurrentLimitStarter.java
+++ spring-redis/src/main/java/com/ternence/spring/redis/CurrentLimitStarter.java
@@ -5,18 +5,22 @@ import com.ternence.spring.redis.cluster.lock.limit.OrderingServiceImpl;
 import com.ternence.spring.redis.cluster.lock.limit.Service;
 import com.ternence.spring.redis.cluster.utils.RedissonUtils;
 import com.ternence.spring.redis.utils.SpringContextHolder;
-import org.redisson.RedissonSemaphore;
 import org.redisson.api.RSemaphore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * 限流功能启动器
+ * 限流功能启动器,redisson提供的RSemaphore并非像java内置的Semaphore一样，
+ * redisson的是非线程安全的，一旦有很多线程去访问这个RSemaphore，你就会看到令人惊讶的现象
+ * rSemaphore.release();就是奖=将那个key加一
+ * rSemaphore.tryAcquire();就是将这个key减一
+ * 所以控制不好的话就会导致明明只设置了是个锁,却好像有几十个锁一样
+ *
+ * 你必须在获取锁的代码块内释放信号量，否则将造成问题
  *
  * @author Ternence
  * @version 1.0
@@ -35,39 +39,51 @@ public class CurrentLimitStarter {
         }));
         LOGGER.info("The application has been started !");
 
-        testSemaphore();
+        testBatch();
+    }
 
-       /* ExecutorService executor = Executors.newCachedThreadPool();
-        for (int i = 0; i < 100; i++) {
+    private static void testBatch() {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        for (int i = 0; i < 50; i++) {
             //提交一百个任务
             executor.submit(new OrderThread());
         }
-        executor.shutdown();//直到前面的任务执行完毕之前不再接受新的任务*/
+        executor.shutdown();//直到前面的任务执行完毕之前不再接受新的任务
     }
 
-    private static void testSemaphore() {
-
+    private static void testDrain() {
         RSemaphore rSemaphore = RedissonUtils.getSemaphore("my-semaphore");
         if (rSemaphore.isExists()) rSemaphore.delete();
 
         if (rSemaphore.trySetPermits(10)) {
             LOGGER.info("信号量设置成");
         }
+        int allPermits = rSemaphore.drainPermits();
+        System.out.println(allPermits);
+    }
+
+    private static void testSemaphore() {
+
+        RSemaphore rSemaphore = RedissonUtils.getSemaphore("my-semaphore");
+        if (rSemaphore.isExists()) {
+            rSemaphore.delete();
+        }
+
+        if (rSemaphore.trySetPermits(10)) {
+            LOGGER.info("信号量设置成10");
+        }
 
         ExecutorService executorService = Executors.newCachedThreadPool();
-
         for (int i = 0; i < 50; i++) {//如果许可不够,他会自动的增加许可数量
             executorService.submit((Runnable) () -> {
+                Logger logger = LoggerFactory.getLogger(Thread.currentThread().getName());
                 while (true) {
-                    try {
-                        if (rSemaphore.tryAcquire()) {
-                            LOGGER.info("{}获取到许可", Thread.currentThread().getName());
-                        } else {
-                            LOGGER.error("{}获取许可失败,{}", Thread.currentThread().getName(), new Exception("获取许可失败"));
-                        }
-                    } finally {
+                    if (rSemaphore.tryAcquire()) {
+                        logger.info("{}获取到许可", Thread.currentThread().getName());
                         rSemaphore.release();
-                        LOGGER.info("{}释放许可,剩余许可数:{}", Thread.currentThread().getName(), rSemaphore.availablePermits());
+                        logger.info("{}释放许可,剩余许可数:{}", Thread.currentThread().getName(), rSemaphore.availablePermits());
+                    } else {
+                        logger.error("{}获取许可失败", Thread.currentThread().getName());
                     }
                 }
             });
@@ -77,11 +93,11 @@ public class CurrentLimitStarter {
 
     private final static class OrderThread extends Thread {
         private boolean flag = true;
+        private Service<String> service = SpringContextHolder.getBean(OrderingServiceImpl.class);
 
         @Override
         public void run() {
             while (flag) {
-                Service<String> service = SpringContextHolder.getBean(OrderingServiceImpl.class);
                 try {
                     @SuppressWarnings("ConstantConditions")
                     String result = service.service();
--- spring-redis/src/main/java/com/ternence/spring/redis/cluster/lock/limit/OrderingServiceImpl.java
+++ spring-redis/src/main/java/com/ternence/spring/redis/cluster/lock/limit/OrderingServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ternence.spring.redis.cluster.lock.limit;
 
 import com.ternence.spring.redis.cluster.utils.RedissonUtils;
+import org.redisson.api.RBucket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,26 +21,29 @@ public class OrderingServiceImpl implements Service<String> {
     private OrderingServiceCurrentLimiting limiting;
     private ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
 
+
     @Override
     public String service() throws CannotServiceException {
+        if (threadLocal.get() == null) {
+            threadLocal.set(0);
+        }
         try {
             if (limiting.canAccess()) {
-                LOGGER.info("成功获取许可,访问服务");
+                LOGGER.info("成功获取许可,访问服务{}", threadLocal.get());
                 threadLocal.set(threadLocal.get() + 1);
-                RedissonUtils.getStringBucket("PV:" + Thread.currentThread().getName())
-                        .set(String.valueOf(threadLocal.get()));
-                RedissonUtils.release();
-                Thread.sleep(1000 * 3);
+                RBucket<String> stringRBucket = RedissonUtils.getStringBucket("PV:" + Thread.currentThread().getName());
+                stringRBucket.set(String.valueOf(threadLocal.get()));
+                limiting.serviceComplete();
+                LOGGER.info("服务完成,释放许可,还有:{}个许可", limiting.getPermits());
                 return "下单成功";
             } else {
                 throw new CannotServiceException("服务繁忙,清稍后重试");
             }
         } catch (InterruptedException e) {
-            LOGGER.error("请求范文服务发生异常:{}", e);
+            LOGGER.error("请求订单服务发生异常:{}", e);
             throw new CannotServiceException("访问当前不可用");
-        } finally {
-            LOGGER.info("服务完成,释放许可,还有:{}个许可", limiting.getPermits());
-            limiting.serviceComplete();
+        } catch (Exception e) {
+            throw new CannotServiceException("访问当前不可用");
         }
     }
 }
