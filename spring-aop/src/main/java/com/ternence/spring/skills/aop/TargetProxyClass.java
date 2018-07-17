9adb75f318757d4cc034fe5442175889d749f2cc
taojianghang <taojianghang@crnet.com.hk>
2018-07-17 12:45:26 +0800

    增加aop

---
+++ spring-aop/src/main/java/com/ternence/spring/skills/aop/TargetProxyClass.java
@@ -0,0 +1,9 @@
+package com.ternence.spring.skills.aop;
+
+/**
+ * @author taojianghang
+ * @version 2.0
+ * @since 2018/7/13 10:08
+ */
+public class TargetProxyClass {
+}
---
+++ spring-aop/src/main/java/com/ternence/spring/skills/aop/asp/ClassAspects.java
@@ -0,0 +1,15 @@
+package com.ternence.spring.skills.aop.asp;
+
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ * 类的切面
+ *
+ * @author taojianghang
+ * @version 2.0
+ * @since 2018/7/13 10:08
+ */
+@Aspect
+public class ClassAspects {
+
+}
